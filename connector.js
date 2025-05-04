869fe5eed2fdbc0716e5ffb180647e1b82887ed9
liubarets <oleg.liubarets@gmail.com>
2025-05-05 00:01:08 +0300

    Create connector.js

---
+++ connector.js
@@ -0,0 +1,15 @@
+/* global TrelloPowerUp */
+TrelloPowerUp.initialize({
+
+  /* ▶ кнопка в картці */
+  'card-buttons': () => [{
+    text: 'Story Points',
+    icon: 'https://img.icons8.com/ios/20/000000/counter.png',
+    callback: t => t.popup({
+      title: 'Story Points',
+      url: './storyPoints.html',
+      height: 160
+    })
+  }]
+
+});
